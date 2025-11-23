00598ad6bdc09866e6aefc06234de8d70b1cdb0a
zakariadachi <dachiziko@gmail.com>
2025-11-23 15:36:19 +0100

    exercices

--- dataset.js
+++ dataset.js
@@ -820,3 +820,129 @@ function inactiveEmployees(employees){
 }
 
 console.log(inactiveEmployees(employees));
+
+
+
+
+
+
+
+
+
+function averageAge(){
+  let average =0;
+  for (let i = 0; i < employees.length; i++) {
+    average += employees[i].salary;
+    
+  }
+  return average/employees.length;
+
+}
+console.log(averageAge());
+
+
+
+
+
+function highestSalary(){
+  max = 0;
+  for (let i = 0; i < employees.length; i++) {
+    if(employees[i].salary>max)
+      max=employees[i].salary;
+    
+  }
+  return max
+}
+console.log(highestSalary());
+
+
+
+
+
+
+function countEmployeesInDepartment(){
+  let count = 0;
+  for (let i = 0; i < employees.length; i++) {
+    if(employees[i].department=="Développement")
+      count++;
+    
+  }
+  return count;
+
+}
+console.log(countEmployeesInDepartment());
+
+
+
+
+
+function getUniqueDepartments(employees){
+  const arr=[];
+  for (let i = 0; i < employees.length; i++) {
+    let department = employees[i].department;
+    if(!arr.includes(department)){
+      arr.push(employees[i].department);
+    }
+    
+  }
+  return arr;
+}
+console.log(getUniqueDepartments(employees));
+
+
+
+
+
+
+function hasEmployeesWithoutProjects(){
+  for(let i=0 ;i<employees.length; i++){
+      if(employees[i].projects.length===0){
+        return true;
+      }
+    }
+  return false;
+}
+console.log(hasEmployeesWithoutProjects());
+
+
+
+
+
+
+function getEmployeesByProject(){
+  const arr=[];
+  for (let i = 0; i < employees.length; i++) {
+    for (let j = 0; j < employees[i].projects.length; j++) {
+      if(employees[i].projects[j]==="Project Gamma"){
+        arr.push(employees[i]);
+
+      }
+      
+    }
+    
+  }
+  return arr;
+}
+console.log(getEmployeesByProject())
+
+
+
+
+function getEmployeesWithMoreThanTwoSkills(){
+  const arr=[];
+  for (let i = 0; i < employees.length; i++) {
+    for (let j = 0; j < employees[i].skills.length; j++) {
+      if (employees[i].skills[j].length>2) {
+        arr.push(employees[i])
+        
+      }
+    }
+    return arr
+    
+  }
+}
+console.log(getEmployeesWithMoreThanTwoSkills())
+
+
+
+
